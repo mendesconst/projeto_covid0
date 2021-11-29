c9c1d927bce3626531d8bab41f8e5f6ff16e9fbe
Jonathan <mendesconst@gmail.com>
2021-11-29 16:09:10 -0300

    Projeto covid0 Versão 2.2.5  Deploy no Heroku

--- plataforma_cursos/settings/base.py
+++ plataforma_cursos/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,3 +132,10 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+
+
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
---
+++ plataforma_cursos/settings/heroku.py
@@ -0,0 +1,16 @@
+import environ
+
+from plataforma_cursos.settings.base import *
+
+env = environ.Env()
+
+DEBUG = env.bool("DEBUG", False)
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+DATABASES = {
+    "default": env.db(),
+
+
