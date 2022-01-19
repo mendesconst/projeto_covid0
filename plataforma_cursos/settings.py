1444a561dfbe201bccc0e1c4dd87c8146b7afb1c
Jonathan <mendesconst@gmail.com>
2022-01-19 20:31:47 -0300

    projetocovid0

--- plataforma_cursos/settings.py
+++ plataforma_cursos/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+
 import os
+import django_heroku
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +133,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
