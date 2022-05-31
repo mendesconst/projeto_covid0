99dec52badd08f4acb275a263c2ea327f4d7fe43
Jofre Carneiro <jofre.carneiro@gmail.com>
2022-05-30 21:30:09 -0300

    Página inicial de login carregando como startpage

--- plataforma_cursos/urls.py
+++ plataforma_cursos/urls.py
@@ -4,10 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from usuarios.views import login
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('home/', include('cursos.urls')),
+    path('', login, name='home')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
