1e4cdf9d0c93bba55732f2b8890234ca8fa4aabb
Jofre Carneiro <jofre.carneiro@gmail.com>
2022-06-03 18:24:34 -0300

    Heroku postgresql setup

--- plataforma_cursos/settings.py
+++ plataforma_cursos/settings.py
@@ -134,4 +134,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
