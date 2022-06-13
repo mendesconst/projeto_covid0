4fee92f4e4d1152e1e9a22405b6208d916d5da68
Jofre Carneiro <jofre.carneiro@gmail.com>
2022-06-12 22:21:49 -0300

    add tables and whitenoise plugin for statics files

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from cursos.models import Cursos 
 
+class TokenCadastro(models.Model):
+    token_cadastro=models.CharField(max_length=8)
+
+    def __str__(self) -> str:
+        return self.token_cadastro
+
 class Usuario(models.Model):
     nome=models.CharField(max_length=50)
     email=models.EmailField()
     senha=models.CharField(max_length=64)
     curso_selecionado=models.ForeignKey(Cursos, on_delete=models.DO_NOTHING, null=True)
+    token_cadastro=models.ForeignKey(TokenCadastro, on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self) -> str:
         return self.nome
 
-        
