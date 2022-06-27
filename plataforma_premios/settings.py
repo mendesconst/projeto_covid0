24de1b83d75c15842bf47612b57dbca42922d247
Jofre Carneiro <jofre.carneiro@gmail.com>
2022-06-27 17:30:17 -0300

    Whitenoise add media in pref

--- plataforma_premios/settings.py
+++ plataforma_premios/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),os.path.join(BASE_DIR, 'media'))
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
